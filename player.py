4559113723a6cdc3adf745c4dab6b837de4fd327
Marko Pap <marko.pap@novelic.com>
2023-08-25 11:35:50 +0200

    Added midi2audio

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+from midi2audio import FluidSynth
 
 def play_midi(file_path):
     pygame.init()
@@ -9,6 +10,16 @@ def play_midi(file_path):
     while pygame.mixer.music.get_busy():
         time.sleep(1)
 
+def midi_to_wav(midi_file, wav_file):
+    sf2_path = r'GeneralUser GS v1.471.sf2'
+    
+    fs = FluidSynth(sf2_path)
+    fs.midi_to_audio(midi_file, wav_file)
+
 if __name__ == '__main__':
-    midi_file = 'generated_midi.mid'
-    play_midi(midi_file)
+    midi_file = 'generated_midi_2.mid'
+    wav_file = 'generated_music_2.wav'
+
+    midi_to_wav(midi_file, wav_file)
+
+    # play_midi(midi_file)
